7e1a3f5f3efc0774e9feb8107ae439f0eb309cfa
Jonas Sjogren <jonas.sjogren@iths.se>
2022-10-13 09:34:38 +0200

    JonasS first commit

--- app/src/main/java/com/example/palfinder/MainActivity.kt
+++ app/src/main/java/com/example/palfinder/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //JonasS first Push on this branch
     }
 }
