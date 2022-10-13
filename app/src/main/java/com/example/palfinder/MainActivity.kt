0177bdf1b7b1cb3f203b1614f0fe41c7eeef42ae
Jonas Sjogren <jonas.sjogren@iths.se>
2022-10-13 09:36:57 +0200

    JonasS second commit and push

--- app/src/main/java/com/example/palfinder/MainActivity.kt
+++ app/src/main/java/com/example/palfinder/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //JonasS first Push on this branch
+        //JonasS second commit and push
     }
 }
