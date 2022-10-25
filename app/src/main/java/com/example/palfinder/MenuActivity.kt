ec0a09b036d2274fa6197c830c17fe39352d86c9
Michael Bergsten <michaelbergsten@Michaels-MacBook-Air.local>
2022-10-25 13:54:38 +0200

    menuActivity

--- app/src/main/java/com/example/palfinder/MenuActivity.kt
+++ app/src/main/java/com/example/palfinder/MenuActivity.kt
@@ -3,7 +3,7 @@ package com.example.palfinder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MenuLayout : AppCompatActivity() {
+class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_layout)
