1eea59c5e0e2598b29019049e7404ea5935df662
Michael Bergsten <michaelbergsten@Michaels-MacBook-Air.local>
2022-11-08 12:10:28 +0100

    profiledatatofirestore

--- app/src/main/java/com/example/palfinder/Fragments/PersonProfileEditFragment.kt
+++ app/src/main/java/com/example/palfinder/Fragments/PersonProfileEditFragment.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.palfinder.R
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class PersonProfileEditFragment : Fragment() {
@@ -16,29 +21,74 @@ class PersonProfileEditFragment : Fragment() {
     lateinit var et_interest: EditText
     lateinit var et_email:EditText
 
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_person_profile_edit, container, false)
+
+
+
     }
 
-    fun viewInitializations() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        et_name = requireView().findViewById(R.id.et_name)
-        et_age= requireView().findViewById(R.id.et_age)
-        et_email  = requireView().findViewById(R.id.et_email)
-        et_interest = requireView().findViewById(R.id.et_interest)
+        et_name = view.findViewById(R.id.et_name)
+        et_age = view.findViewById(R.id.et_age)
+        et_email = view.findViewById(R.id.et_email)
+        et_interest = view.findViewById(R.id.et_interest)
+
+        val db = Firebase.firestore
+
+        val updateButton = view.findViewById<Button>(R.id.button_register)
+
+        updateButton.setOnClickListener {
+            val person = hashMapOf(
+                "First Name" to et_name.text.toString(),
+                "Last Name" to et_email.text.toString(),
+                "Age" to et_age.text.toString().toIntOrNull(),
+                "Email" to et_email.text.toString(),
+                "Interests" to et_interest.text.toString()
+            )
+
+            db.collection("users")
+                .add(person)
+                .addOnSuccessListener { documentReference ->
+                    Toast.makeText(view.context, "Success", Toast.LENGTH_LONG).show()
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(view.context, "Failed", Toast.LENGTH_LONG).show()
+                }
+
+        }
+
+    }
+
+
+    }
+
+
+ //   fun viewInitializations() {
+
+ //       et_name = requireView().findViewById(R.id.et_name)
+ //       et_age= requireView().findViewById(R.id.et_age)
+ //       et_email  = requireView().findViewById(R.id.et_email)
+//      et_interest = requireView().findViewById(R.id.et_interest)
 
 
         // To show back button in actionbar
         // supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-    }
 
 
-    }
+
+   // }
 
     // Checking if the input in form is valid
    /* fun validateInput(): Boolean {
@@ -90,7 +140,9 @@ class PersonProfileEditFragment : Fragment() {
         }
     }
 
-    */
+     */
+
+    
 
 
 
