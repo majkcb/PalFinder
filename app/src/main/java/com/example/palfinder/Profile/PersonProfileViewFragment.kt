2ccf6e70272ff7faab23494212728504aae4faa2
WT <warat@hotmail.se>
2022-11-11 14:35:13 +0100

    fixedLayout

---
+++ app/src/main/java/com/example/palfinder/Profile/PersonProfileViewFragment.kt
@@ -0,0 +1,24 @@
+package com.example.palfinder.Profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.palfinder.R
+
+
+class PersonProfileViewFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_person_profile_view, container, false)
+    }
+
+    companion object {
+
+    }
+}
